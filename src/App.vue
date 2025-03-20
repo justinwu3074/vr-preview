e080f62d2ef1f985d95eab25320bf572bc0f347a
justinwu3074 <wuj3074@gmail.com>
2025-03-20 18:33:17 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Home ></Home>
+</template>
+
+<script setup lang="ts">
+
+import { onMounted } from 'vue';
+import Home from './views/Home.vue'
+onMounted(() => {
+
+})
+
+</script>
+
+<style scoped></style>
